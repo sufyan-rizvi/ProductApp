39023aade5a74379a5c521386ea36df70bc14430
sufyan-rizvi <sofylaiq@gmail.com>
2024-07-12 11:24:38 +0530

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Models
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _discountPercent;
+        private float _priceAfterDiscount;
+
+        public int GetId() { return _id; }
+        public string GetName() { return _name; }
+        public int GetPrice() { return _price; }
+        public int GetDiscount() { return _discountPercent; }
+        public void SetId(int id) { _id = id; }
+        public void SetName(string name) { _name = name; }
+        public void SetPrice(int price) {  _price = price; }
+        public void SetDiscount(int discountPercent) {  _discountPercent = discountPercent; }
+
+        //public float GetPriceAfterDiscount() {  return _priceAfterDiscount; }
+
+        public float CalculatePriceAfterDiscount()
+        {
+
+
+            _priceAfterDiscount = _price - (_price * _discountPercent / 100);
+            return _priceAfterDiscount;
+            
+            
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            Product product2 = new Product();
+
+            product1.SetName("Shampoo");
+            product1.SetPrice(500);
+            product1.SetId(32165);
+            product1.SetDiscount(10);
+            
+             
+            product2.SetName("Soap");
+            product2.SetPrice(60);
+            product2.SetId(81564);
+            product2.SetDiscount(15);
+
+            PrintItems(product1);
+            PrintItems(product2);
+
+
+
+        }
+
+        public static void PrintItems(Product product)
+        {
+            Console.WriteLine($"Product1 name: {product.GetName()}");
+            Console.WriteLine($"Product1 price: {product.GetPrice()}");
+            Console.WriteLine($"Product1 id: {product.GetId()}");
+            Console.WriteLine($"Product1 discount: {product.GetDiscount()}");
+            Console.WriteLine($"Product1 price after discount: {product.CalculatePriceAfterDiscount()}");
+        }
+
+
+
+    }
+}
